4b6e4f83d79addd95f28fd0d521ef12adb0e9bd8
Lucas <lk_gamer@hotmail.com>
2018-08-09 14:12:47 -0300

    Project Added

--- js/quantidade.js
+++ js/quantidade.js
@@ -32,3 +32,76 @@ $(document).ready(function() {
 		contador.text(contagem - 1);
 	};
 });
+
+
+$(document).ready(function() {
+	var contador = $('#quantidade2 .contador span');
+
+	/* Incremento do contador */
+
+	var btn_quantidade_mais = $('#quantidade-mais2');
+
+	btn_quantidade_mais.on('click', funcao_incrementa);
+
+	/* Decremento do contador */
+
+	var btn_quantidade_menos = $('#quantidade-menos2');
+
+	btn_quantidade_menos.on('click', funcao_decrementa);
+
+
+	/* Funções de incremento e decremento */
+
+	function funcao_incrementa() {
+		var contagem = parseInt(contador.text());
+		
+		contador.text(contagem + 1);
+	};
+
+	function funcao_decrementa() {
+		var contagem = parseInt(contador.text());
+
+		if(contagem <= 0){
+			return;
+		}
+
+		contador.text(contagem - 1);
+	};
+});
+
+$(document).ready(function() {
+	var contador = $('#quantidade3 .contador span');
+
+	/* Incremento do contador */
+
+	var btn_quantidade_mais = $('#quantidade-mais3');
+
+	btn_quantidade_mais.on('click', funcao_incrementa);
+
+	/* Decremento do contador */
+
+	var btn_quantidade_menos = $('#quantidade-menos3');
+
+	btn_quantidade_menos.on('click', funcao_decrementa);
+
+
+	/* Funções de incremento e decremento */
+
+	function funcao_incrementa() {
+		var contagem = parseInt(contador.text());
+		
+		contador.text(contagem + 1);
+	};
+
+	function funcao_decrementa() {
+		var contagem = parseInt(contador.text());
+
+		if(contagem <= 0){
+			return;
+		}
+
+		contador.text(contagem - 1);
+	};
+});
+
+
